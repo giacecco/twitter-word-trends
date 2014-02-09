de17e1e32000d099c81c97fd8b4383b5bc6e5cb0
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-02-09 23:20:29 +0000

    Renamed

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 var async = require("async"),
+	argv = require("optimist")
+		.usage('Usage: $0 [--reset]')
+		.argv;
     twitter = require("./twitter"),
     google = require("./google");
 
-async.series([
-    twitter.initialise,
-    google.initialise
-], function (err) {
+function mainLoop (err) {
     twitter.listen([ "#valentinesday"], function (words) {
         process.stdout.write(Array(words.length + 1).join("."));
         google.writeWords(words);
     });
-})
+}
 
+async.parallel([
+    twitter.initialise,
+    google.initialise, 
+], function (err) {
+	if (argv.reset) {
+		google.resetTable(mainLoop);
+	} else {
+		mainLoop(err);
+	}
+});
