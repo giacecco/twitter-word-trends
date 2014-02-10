cd39aec82dc9a5444374c97e85ecaea7d49bd1a1
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-02-10 09:52:10 +0000

    Issues CSV from web server

---
+++ in_memory.js
@@ -0,0 +1,37 @@
+var csv = require("csv"),
+	db = { };
+
+exports.initialise = function (callback) {
+	callback(null);
+}
+
+exports.writeWords = function (words, callback) {
+	words.forEach(function (word) {
+		if (typeof(db[word.created_at]) === "undefined") {
+			db[word.created_at] = { }
+		} 
+		db[word.created_at][word.word] = (db[word.created_at][word.word] || 0) + 1;
+	});
+	if (callback) callback(null);
+}
+
+exports.toCSV = function (callback) {
+	var allWords = Object.keys(db).reduce(function (memo, timestamp) {
+		var wordsInTimestamp = Object.keys(db[timestamp]);
+		return wordsInTimestamp.reduce(function (memo2, word) {
+			return memo2.indexOf(word) === -1 ? memo2.concat(word) : memo2;
+		}, memo);
+	}, [ ]).sort();
+	csv()
+		.from.array(Object.keys(db).map(function (timestamp) {
+			var temp = db[timestamp];
+			temp.timestamp = timestamp;
+			return temp;
+		}))
+		.to.string(function (data, count) {
+			callback(null, data);
+		}, {
+			header: true, 
+			columns: [ "timestamp"].concat(allWords)
+		});
+}
--- main.js
+++ main.js
@@ -3,22 +3,38 @@ var async = require("async"),
 		.usage('Usage: $0 [--reset]')
 		.argv;
     twitter = require("./twitter"),
-    google = require("./google");
+    google = require("./google"),
+    inMemory = require("./in_memory");
 
 function mainLoop (err) {
     twitter.listen([ "#valentinesday"], function (words) {
         process.stdout.write(Array(words.length + 1).join("."));
-        google.writeWords(words);
+        inMemory.writeWords(words);
+        // google.writeWords(words);
     });
+}
+
+function launchWebServer () {
+	var http = require('http');
+	http.createServer(function (req, res) {
+		inMemory.toCSV(function (err, csv) {
+			res.writeHead(200, { 'Content-Type': 'text/csv' });
+			res.end(csv);
+		});
+	}).listen(9615);	
 }
 
 async.parallel([
     twitter.initialise,
+    inMemory.initialise, 
     google.initialise, 
 ], function (err) {
 	if (argv.reset) {
 		google.resetTable(mainLoop);
 	} else {
-		mainLoop(err);
+		async.parallel([
+			mainLoop,
+			launchWebServer
+		]);
 	}
 });
