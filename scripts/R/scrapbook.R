979415ddaede9c6c643654e2d7dea69db76917ff
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-02-16 11:06:58 +0000

    Fixed wrong type for timestamp

--- scripts/R/scrapbook.R
+++ scripts/R/scrapbook.R
@@ -1,1 +1,16 @@
-temp <- tbl_df(read.csv(pipe("curl http://localhost:8080/data/"), header=T))
+library(dplyr)
+
+fetchReport <- function (interval = NA, limit = NA, other = FALSE) {
+    command = "curl 'http://localhost:8080/data/?"
+    if (!is.na(interval)) { command = paste0(command, "interval=", interval, "&") }
+    if (!is.na(limit)) { 
+        command = paste0(command, "limit=", limit, "&") 
+        if (other) { command = paste0(command, "other&") }
+    }
+    command = paste0(command, "'")
+    temp <- tbl_df(read.csv(pipe(command), header = TRUE))
+    # force the timestamp column to POSIXct, which dplyr can work with
+    temp$timestamp <- as.POSIXct(strptime(temp$timestamp, "%Y-%m-%d %H:%M:%S"))
+    return(temp)
+}
+
